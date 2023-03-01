0c10579cf66c655054da2d052a5904a56d03cea6
Victoria <vmcnorrill@gmail.com>
2023-03-01 17:51:10 -0500

    JS: added code to displayCity funciton

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,31 @@ var apiURLWithKey = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon
 var apiKey = '173d4962b14a682100b481e4cceca14d';
 var savedSearch = [];
 
+//HTML Elements
+let cityWeatherDiv = $('#cityweather');
+
+
 //BEGIN WORKING CODE
 
 //Type a City and CLICK "searchbtn"
+$('#searchbtn').on('click', displayCity)
 
-    //Display Current Conditions
-        //City Name
-        //Current Date
-        //Icon with representing weather conditions
-            //Temperature
-            //Wind
-            //Humidity
+//Function to Display Current Conditions
+let displayCity = function () {
+    //City Name
+    var city = 
+    //Current Date day.js
+    var today = dayjs('dddd, MM/DD/YYYY')
+    cityWeatherDiv.append( $('h2', today)).addClass('currentdate');
+
+
+    //Icon with representing weather conditions
+
+    //Temperature
+    //Wind
+    //Humidity
+}
+
 
     //Display Five-Day Forecast
         //Each Forecast will Have:
