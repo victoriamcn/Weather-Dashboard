cc041bed57684db4bc09ed6c799b310fce66b068
Victoria <vmcnorrill@gmail.com>
2023-02-28 23:19:24 -0500

    added API Key for five day weather forecaste server-sid api. JS: commented out acceptance requirements for the code

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var apiURLWithKey = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={173d4962b14a682100b481e4cceca14d}'
+var apiKey = '173d4962b14a682100b481e4cceca14d';
+var savedSearch = []
+
+//BEGIN WORKING CODE
+
+//Type a City and CLICK "searchbtn"
+
+    //Display Current Conditions
+        //City Name
+        //Current Date
+        //Icon with representing weather conditions
+            //Temperature
+            //Wind
+            //Humidity
+
+    //Display Five-Day Forecast
+        //Each Forecast will Have:
+            //City Name
+            //Current Date
+            //Icon with representing weather conditions
+                //Temperature
+                //Wind
+                //Humidity
+
+//That city is added to localStorage
+
+//That city is displayed individually in the "citiesfromstorage" div
+
+//Click city "searchhistory" btn
