1ea70101f549a6c1724aec9153238470e0e59de8
Victoria <vmcnorrill@gmail.com>
2023-03-01 19:37:33 -0500

    JS: wrote geoLocation function to get lat/lon. wrote fetchForecast funciton to get the weather data. with tutor. next: write function to json() the weather and display as a list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,53 @@
-var apiURLWithKey = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={173d4962b14a682100b481e4cceca14d}'
+
 var apiKey = '173d4962b14a682100b481e4cceca14d';
 var savedSearch = [];
 
-//HTML Elements
-let cityWeatherDiv = $('#cityweather');
-
-
 //BEGIN WORKING CODE
+// fetching the lat/lon 
+function geoLocation(nameofcity) {
+    var apiCityURL = `http://api.openweathermap.org/geo/1.0/direct?q=${nameofcity}&limit=1&appid=${apiKey}`
+    fetch(apiCityURL)
+        .then(function (response) {
+            // console.log(response)
+            return response.json()
+        })
+        .then(function (data) {
+            console.log("DATA of geolocation: ", data)
+            console.log("lat : ", data[0].lat);
+            console.log("lon : ", data[0].lon);
+            getForecast(data[0].lat, data[0].lon)
+        })
+}
+// fetching the forecast for the lat/lon
+function getForecast(lat, lon) {
+    var apiForecastURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(apiForecastURL)
+        .then(function (response) {
+            // console.log(response)
+            return response.json()
+        })
+        .then(function (data) {
+            console.log("DATA of forecast: ", data)
 
-//Type a City and CLICK "searchbtn"
-$('#searchbtn').on('click', displayCity)
+        })
+}
 
 //Function to Display Current Conditions
 let displayCity = function () {
     //City Name
-    var city = 
+
+    //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={apiKey}
     //Current Date day.js
-    var today = dayjs('dddd, MM/DD/YYYY')
-    cityWeatherDiv.append( $('h2', today)).addClass('currentdate');
+    let today = dayjs().format('dddd, MM/DD/YYYY')
+    let city = $('.form-control').val()
+    geoLocation(city)
+
+    //clear before append
+    $('#cityweather').empty();
+    //$('#cityweather').addClass('currentdate');
+    $('#cityweather').append(`<h2 class="currentdate">${today}</h2>`);
+    $('#cityweather').append(`<h2 class="city">${city}</h2>`);
+
 
 
     //Icon with representing weather conditions
@@ -27,6 +57,8 @@ let displayCity = function () {
     //Humidity
 }
 
+//Type a City and CLICK "searchbtn"
+$('#searchbtn').on('click', displayCity);
 
     //Display Five-Day Forecast
         //Each Forecast will Have:
